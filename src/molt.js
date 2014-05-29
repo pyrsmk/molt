3ae0294286a366138587926a68109f47cce63c3a
pyrsmk <dev@dreamysource.fr>
2014-05-29 09:58:14 +0200

    Fix promises behavior

--- src/molt.js
+++ src/molt.js
@@ -18,8 +18,6 @@
 }(this,'molt',function(){
 
 	var images=[],
-		loading,
-		loaded,
 		Promises={
 			promises: {
 				early: [],
@@ -37,98 +35,110 @@
 				}
 			}
 		},
-
-	// Core function
-	refresh=function(){
-		// Prepare
-		var width=W(),
-			i,j,k,l,
-			attributes,
-			url,
-			mode,
-			modes,
-			img;
-		loading=0;
-		loaded=0;
-		// Launch early promises
-		Promises.run('early',{
-			mode : mode,
-			images : images
-		});
-		// Browse images
-		for(i=0,j=images.length;i<j;++i){
+		loading,
+		loaded,
+		cache=[],
+		getOnLoad=function(mode,img){
+			return function(){
+				++loaded;
+				cache.push(img.getAttribute('src'));
+				Promises.run('each',{
+					mode : mode,
+					image : img
+				});
+				if(loaded>=loading){
+					Promises.run('then',{
+						mode : mode,
+						images : images
+					});
+				}
+			};
+		},
+		getOnError=function(url){
+			return function(){
+				throw "An error occured when loading '"+url+"'";
+			};
+		},
+		// Core function
+		refresh=function(){
 			// Prepare
-			img=images[i];
-			modes=[];
-			url='';
-			attributes=img.attributes;
-			mode=0;
-			// Discover modes
-			if(attributes['data-src']){
-				modes=attributes['data-src'].value.match(/\{(.+?)\}/)[1].split(/\s*,\s*/);
-			}
-			else{
-				for(k=0,l=attributes.length;k<l;++k){
-					if(/^data-\d+$/i.test(attributes[k].name)){
-						modes.push(parseInt(attributes[k].name.substring(5),10));
+			var width=W(),
+				i,j,k,l,
+				attributes,
+				url,
+				mode,
+				modes,
+				img;
+			loading=0;
+			loaded=0;
+			// Launch early promises
+			Promises.run('early',{
+				mode : mode,
+				images : images
+			});
+			// Browse images
+			for(i=0,j=images.length;i<j;++i){
+				// Prepare
+				img=images[i];
+				modes=[];
+				url='';
+				attributes=img.attributes;
+				mode=0;
+				// Discover modes
+				if(attributes['data-src']){
+					modes=attributes['data-src'].value.match(/\{(.+?)\}/)[1].split(/\s*,\s*/);
+				}
+				else{
+					for(k=0,l=attributes.length;k<l;++k){
+						if(/^data-\d+$/i.test(attributes[k].name)){
+							modes.push(parseInt(attributes[k].name.substring(5),10));
+						}
 					}
 				}
-			}
-			// Sort modes
-			modes.sort(function(a,b){
-				return a-b;
-			});
-			// Find which URL to load
-			for(k=0,l=modes.length;k<l;++k){
-				if(width>=modes[k]){
-					mode=modes[k];
-					if(attributes['data-src']){
-						url=attributes['data-src'].value.replace(/\{.+?\}/,modes[k]);
+				// Sort modes
+				modes.sort(function(a,b){
+					return a-b;
+				});
+				// Find which URL to load
+				for(k=0,l=modes.length;k<l;++k){
+					if(width>=modes[k]){
+						mode=modes[k];
+						if(attributes['data-src']){
+							url=attributes['data-src'].value.replace(/\{.+?\}/,modes[k]);
+						}
+						else{
+							url=attributes['data-'+modes[k]].value;
+						}
+					}
+				}
+				// Load image
+				if(url && url!=img.src){
+					++loading;
+					if(cache.indexOf(url)==-1){
+						if(!img.onload){
+							img.onload=getOnLoad(mode,img);
+							img.onerror=getOnError(url);
+						}
+						img.src=url;
 					}
 					else{
-						url=attributes['data-'+modes[k]].value;
+						img.src=url;
+						getOnLoad(mode,img)();
+					}
+					if(img.style.visibility!='visible'){
+						img.style.visibility='visible';
+						img.style.width='auto';
+						img.style.height='auto';
 					}
 				}
-			}
-			// Load image
-			if(url && url!=img.src){
-				++loading;
-				img.src=img.src; // Prevents IE9 onload bug
-				img.onload=function(mode,img){
-					return function(e){
-						++loaded;
-						Promises.run('each',{
-							mode : mode,
-							image : img
-						});
-						if(loaded==loading){
-							Promises.run('then',{
-								mode : mode,
-								images : images
-							});
-						}
-					};
-				}(mode,img);
-				img.onerror=function(url){
-					return function(){
-						throw "An error occured when loading '"+url+"'";
-					};
-				}(url);
-				img.src=url;
-				if(img.style.visibility!='visible'){
-					img.style.visibility='visible';
-					img.style.width='auto';
-					img.style.height='auto';
+				// Hide image
+				else if(img.style.visibility!='hidden'){
+					img.style.visibility='hidden';
+					img.style.width=0;
+					img.style.height=0;
 				}
 			}
-			// Hide image
-			else if(img.style.visibility!='hidden'){
-				img.style.visibility='hidden';
-				img.style.width=0;
-				img.style.height=0;
-			}
-		}
-	};
+		};
 
 	// Add new nodes to the stack
 	return function(nodes){
